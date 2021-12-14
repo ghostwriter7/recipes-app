9ef17614cd571cd5b7ae1b4598d3e01ce53687a3
Hubert Rozplochowski <Hubert.Rozplochowski@createctrl.com>
2021-12-14 12:46:40 +0100

    create recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    { name: 'veggies', description: 'delicious as hell, trust me', imagePath: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' },
+    { name: 'reindeer meat', description: 'it is very nutritious, and cheaper than chicken', imagePath: 'https://images.unsplash.com/photo-1610389444027-27466f27b0a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' },
+    { name: 'pizza', description: 'simple as it, damn pizza, tastes good', imagePath: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' }
+  ];
 
   constructor() { }
 
